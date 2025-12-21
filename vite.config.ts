f82e1b891c5a369091edc96067a1c45404f9f44c
Thiago Mata <thiago.henrique.mata@gmail.com>
2025-12-22 03:35:47 +1100

    codecov config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config'; 
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 export default defineConfig({
   base: '/parallax/', 
+  plugins: [
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "parallax-camera",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   test: {
-    // This ensures your tests run in a browser-like environment if needed
     environment: 'happy-dom', 
     coverage: {
       provider: 'v8',
-      // 'text' gives you the terminal table, 'html' gives you the website
       reporter: ['text', 'html', 'json', 'json-summary'],
-      // This is the important part for seeing "missing" lines
       include: ['src/**/*.ts'],
       exclude: ['src/main.ts', 'src/**/*.test.ts'],
       thresholds: {
